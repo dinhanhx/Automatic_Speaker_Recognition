ecb045e589ff754a34c44ee0232cd59abe4b0f6e
quangLH195 <59214933+quangLH195@users.noreply.github.com>
2020-06-05 21:45:23 +0700

    Create train_model.py

---
+++ source_code/LHQ/train_model.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('../') # To import all python files in `source_code` folder.
+from mfcc_gmm_func import * # To import functions that you need
+
+import glob
+audio_file_path_list = glob.glob('../../datasets/LHQ/*.wav')
+
+model_path = 'LHQ.gmm'
+
+generate_GMM(audio_file_path_list, model_path)
+
+for audio_file_path in audio_file_path_list:
+    print(try_personal_GMM(audio_file_path, model_path))
