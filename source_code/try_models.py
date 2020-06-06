b8238dec0038d6109355687b71d59a56bc28366d
dinhanhx <dinhanhx@gmail.com>
2020-06-06 20:23:42 +0700

    Clean up old models

--- source_code/try_models.py
+++ source_code/try_models.py
@@ -3,25 +3,22 @@ from math import inf
 import pickle
 import os
 
-names = ['VDA', 'NNDH', 'NQT', 'TQD', 'LHQ', 'LHN', 'DGL', 'TMH']
+names = ['VDA', 'NNDH', 'NQT', 'LHQ', 'TQD', 'LHN', 'TMH', 'DGL']
 
 model_path_list = [name + '/' + name + '.gmm' for name in names]
 
-audio_file_path = '../datasets/VDA/VDA_1.wav'
+audio_file_path = '../datasets/VDA/notinlab.wav'
 
-likelyhood_score_list = []
+likelihood_score_list = []
 for model_path in model_path_list:
     if os.path.exists(model_path):
-        likelyhood_score_list.append(round(try_personal_GMM(audio_file_path, model_path), 5))
+        likelihood_score_list.append(round(try_personal_GMM(audio_file_path, model_path), 5))
     else:
-        likelyhood_score_list.append(-inf)
+        likelihood_score_list.append(-inf)
 
 
-print('The file is')
-print(audio_file_path)
-print()
-print('The likelyhood score list is')
-print(likelyhood_score_list)
-print()
-print('The person is')
-print(names[likelyhood_score_list.index(max(likelyhood_score_list))])
+print('The file: ' + audio_file_path + '\n')
+print('The likelihood score list: ')
+name_likelihood_score = [name + ': ' + str(likelihood_score) for name, likelihood_score in zip(names, likelihood_score_list)] 
+print(*name_likelihood_score, sep = '\n')
+print('\n' + 'The likelhood person: '+names[likelihood_score_list.index(max(likelihood_score_list))])
