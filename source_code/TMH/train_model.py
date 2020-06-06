7d921d2f17ee457843bb89913e5de415f4a3cd02
pcranger <tmhieu2000@gmail.com>
2020-06-06 08:39:42 +0700

    add tran_model.py

---
+++ source_code/TMH/train_model.py
@@ -0,0 +1,14 @@
+import sys
+import glob
+sys.path.append('../') # To import all python files in `source_code` folder.
+from mfcc_gmm_func import * # To import functions that you need
+
+
+audio_file_path_list = glob.glob('../../datasets/TMH/*.wav')
+
+model_path = 'TMH.gmm'
+
+generate_GMM(audio_file_path_list, model_path)
+
+for audio_file_path in audio_file_path_list:
+    print(try_personal_GMM(audio_file_path, model_path))
