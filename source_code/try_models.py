455c0c0ba77fb4b594d5ba1ee60886fc755509d9
dinhanhx <dinhanhx@gmail.com>
2020-06-07 08:25:37 +0700

    Refactor to be ready

--- source_code/try_models.py
+++ source_code/try_models.py
@@ -1,24 +1,23 @@
 from mfcc_gmm_func import try_personal_GMM
 from math import inf
-import pickle
 import os
 
+audio_file_path = ''
+model_type = ''
+
 names = ['VDA', 'NNDH', 'NQT', 'LHQ', 'TQD', 'LHN', 'TMH', 'DGL']
+model_path_list = ['models/'+model_type+'/'+name+'.gmm' for name in names]
 
-model_path_list = [name + '/' + name + '.gmm' for name in names]
-
-audio_file_path = '../datasets/VDA/notinlab.wav'
-
-likelihood_score_list = []
+scores = []
 for model_path in model_path_list:
     if os.path.exists(model_path):
-        likelihood_score_list.append(round(try_personal_GMM(audio_file_path, model_path), 5))
+        scores.append(round(try_personal_GMM(audio_file_path, model_path),5))
     else:
-        likelihood_score_list.append(-inf)
+        scores.append(-inf)
 
 
 print('The file: ' + audio_file_path + '\n')
 print('The likelihood score list: ')
-name_likelihood_score = [name + ': ' + str(likelihood_score) for name, likelihood_score in zip(names, likelihood_score_list)] 
-print(*name_likelihood_score, sep = '\n')
-print('\n' + 'The likelhood person: '+names[likelihood_score_list.index(max(likelihood_score_list))])
+names_scores = [name+': '+str(score) for name, score in zip(names, scores)]
+print(*names_scores, sep = '\n')
+print('\n' + 'The likelhood person: '+names[scores.index(max(scores))])
